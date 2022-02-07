6d682b3ae42dd6e3410dbe7da10b9f4b15a17d7d
Gastón Abalde <abaldeg@gmail.com>
2022-02-07 18:58:42 -0300

    Update CREACION DE LOGIN USUARIO DBUP.sql

--- DBA_SCRIPTS_2021/Security/CREACION DE LOGIN USUARIO DBUP.sql
+++ DBA_SCRIPTS_2021/Security/CREACION DE LOGIN USUARIO DBUP.sql
@@ -1,10 +1,11 @@
 /* For security reasons the login is created disabled and with a random password. */
 /****** Object:  Login [dbupexpress]    Script Date: 2/7/2022 6:44:51 PM ******/
-CREATE LOGIN [dbupexpress] WITH PASSWORD=N'XXX'
+CREATE LOGIN [dbupbodylogictest] WITH PASSWORD=N'W5LrKSFRpkGx67'
 GO
 
 /****** Object:  User [dbupexpress]    Script Date: 2/7/2022 6:48:49 PM ******/
-CREATE USER [dbupexpress] FOR LOGIN [dbupexpress] WITH DEFAULT_SCHEMA=[dbo]
+CREATE USER [dbupbodylogictest] FOR LOGIN [dbupbodylogictest] 
+WITH DEFAULT_SCHEMA=[dbo]
 GO
 
-sp_addrolemember 'db_owner', 'dbupexpress'
+sp_addrolemember 'db_owner', 'dbupbodylogictest'
