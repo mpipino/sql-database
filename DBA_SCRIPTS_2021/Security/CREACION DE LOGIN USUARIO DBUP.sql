516644668fb4f3f914436d6608290eaa0b35ff74
Gastón Abalde <abaldeg@gmail.com>
2022-02-10 20:24:21 -0300

    dbup

--- DBA_SCRIPTS_2021/Security/CREACION DE LOGIN USUARIO DBUP.sql
+++ DBA_SCRIPTS_2021/Security/CREACION DE LOGIN USUARIO DBUP.sql
@@ -1,6 +1,20 @@
+
 /* For security reasons the login is created disabled and with a random password. */
 /****** Object:  Login [dbupexpress]    Script Date: 2/7/2022 6:44:51 PM ******/
-CREATE LOGIN [dbupenvisionarystage] WITH PASSWORD=N'GzU5hAJhvp5T2h'
+CREATE LOGIN [dbupenvisionarydev] WITH PASSWORD=N'w7hgdiXCSTebGT'
+GO
+
+/****** Object:  User [dbupexpress]    Script Date: 2/7/2022 6:48:49 PM ******/
+CREATE USER [dbupenvisionarydev] FOR LOGIN [dbupenvisionarydev] 
+WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+sp_addrolemember 'db_owner', 'dbupenvisionarydev'
+
+
+/* For security reasons the login is created disabled and with a random password. */
+/****** Object:  Login [dbupexpress]    Script Date: 2/7/2022 6:44:51 PM ******/
+CREATE LOGIN [dbupenvisionarystage] WITH PASSWORD=N'6m9AYn9ARWkNiC'
 GO
 
 /****** Object:  User [dbupexpress]    Script Date: 2/7/2022 6:48:49 PM ******/
@@ -11,21 +25,10 @@ GO
 sp_addrolemember 'db_owner', 'dbupenvisionarystage'
 
 
-/* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [dbupexpress]    Script Date: 2/7/2022 6:44:51 PM ******/
-CREATE LOGIN [dbupenvisionarystage] WITH PASSWORD=N'Jgow7V4VUXPpJj'
-GO
-
-/****** Object:  User [dbupexpress]    Script Date: 2/7/2022 6:48:49 PM ******/
-CREATE USER [dbupenvisionarystage] FOR LOGIN [dbupenvisionarystage] 
-WITH DEFAULT_SCHEMA=[dbo]
-GO
-
-sp_addrolemember 'db_owner', 'dbupenvisionarystage'
 
 ------------------------------------
 
-CREATE LOGIN [dbupenvisionarylive] WITH PASSWORD=N'75TaJEjtz7iama'
+CREATE LOGIN [dbupenvisionarylive] WITH PASSWORD=N'S3D9DHnqZYyiJ7'
 GO
 
 /****** Object:  User [dbupexpress]    Script Date: 2/7/2022 6:48:49 PM ******/
