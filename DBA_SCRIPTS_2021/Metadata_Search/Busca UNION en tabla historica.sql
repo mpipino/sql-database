b25e6bc4eb21bd06f9c4f5d1231656f5e6051e4d
Gastón Abalde <abaldeg@gmail.com>
2022-03-02 19:28:31 -0300

    agregue que busque merge

--- DBA_SCRIPTS_2021/Metadata_Search/Busca UNION en tabla historica.sql
+++ DBA_SCRIPTS_2021/Metadata_Search/Busca UNION en tabla historica.sql
@@ -9,3 +9,16 @@ WHERE
 	m.definition LIKE '%tbl_distributor_commissions_v2_Historic%'
 	--AND o.name='FN_GETACTIVEDISTRIBUTOR_XC'
 	--AND o.type_desc = 'SQL_SCALAR_FUNCTION';
+
+
+SELECT DISTINCT o.name AS Object_Name, o.type_desc, 'sp_helptext ''' + o.name + ''''
+FROM sys.sql_modules m
+INNER JOIN
+sys.objects o
+ON m.object_id = o.object_id
+WHERE 
+	m.definition Like '%merge%'
+	
+
+
+
