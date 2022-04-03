92fd3002751b6d8bbc3a9a4b30363f3676f01a33
Gastón Abalde <abaldeg@gmail.com>
2022-04-03 12:30:08 -0300

    Create ASEA_Tiempo_de_calculo_comisiones.sql

---
+++ DBA_SCRIPTS_2021/Monitoring_Activity/ASEA_Tiempo_de_calculo_comisiones.sql
@@ -0,0 +1,8 @@
+SELECT
+      [Commissions_VolumeCalculationLog_Id]
+      
+      ,Min([CreateDate]) as 'start'
+	  ,max([CreateDate]) as 'end'
+  FROM [dbo].[Commissions_VolumeCalculationLog_Detail]
+ group by [Commissions_VolumeCalculationLog_Id]
+ order by [Commissions_VolumeCalculationLog_Id] desc
