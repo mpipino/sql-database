4cf84b14c032227e413a07d5f5a68ccea5989298
mpipino <mpipino@gmail.com>
2023-10-06 15:03:50 -0300

    scripts v3

--- DBA_SCRIPTS_2021/Reports/Index_Fragmentation.sql
+++ DBA_SCRIPTS_2021/Reports/Index_Fragmentation.sql
@@ -15,3 +15,25 @@ INNER JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[objec
 AND indexstats.index_id = dbindexes.index_id
 WHERE indexstats.database_id = DB_ID() 
 ORDER BY indexstats.page_count desc
+
+
+SELECT
+OBJECT_SCHEMA_NAME(ips.OBJECT_ID) 'Schema',
+OBJECT_NAME(ips.OBJECT_ID) 'Table',
+i.NAME,
+ips.index_id,
+index_type_desc,
+avg_fragmentation_in_percent,
+avg_page_space_used_in_percent,
+page_count
+FROM
+sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'SAMPLED') ips
+INNER JOIN
+sys.indexes i
+ON (ips.object_id = i.object_id)
+AND
+(
+ips.index_id = i.index_id
+)
+ORDER BY
+avg_fragmentation_in_percent DESC
